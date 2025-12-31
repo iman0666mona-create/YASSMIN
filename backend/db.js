3e92340b0445ccbe4abf39fbcf02c2828cdf3e19
Ghezlan Lameche <ghezlan@student.dz>
2025-12-31 13:33:08 +0100

    fix: update database connection string to use specific PostgreSQL credentials

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString:  'postgresql://iconic_db_7haq_user:nZLqrHUVe4tW81Xi7Sna3S6LnjiL6VGv@dpg-d5a528emcj7s73c68vu0-a.frankfurt-postgres.render.com/iconic_db_7haq',
   ssl: { rejectUnauthorized: false }
 });
 
