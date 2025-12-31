05e179c3a3e87f9efd7fb2cbfa05c8ee4af73cb2
Ghezlan Lameche <ghezlan@student.dz>
2025-12-31 13:41:08 +0100

    fix

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,17 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: 'postgresql://iconic_db_7haq_user:nZLqrHUVe4tW81Xi7Sna3S6LnjiL6VGv@dpg-d5a528emcj7s73c68vu0-a.frankfurt-postgres.render.com/iconic_db_7haq',
-
   ssl: { rejectUnauthorized: false }
 });
 
-module.exports = {
-  query: (text, params) => pool.query(text, params)
-};
+function query(sql, params, cb) {
+  if (typeof params === 'function') {
+    cb = params;
+    params = [];
+  }
+  pool.query(sql, params)
+    .then(res => cb(null, res.rows))
+    .catch(err => cb(err));
+}
+
+module.exports = { query };
