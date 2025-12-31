844634f99c269c8bce0088f40622068fec1397d5
Ghezlan Lameche <ghezlan@student.dz>
2025-12-31 13:53:12 +0100

    fix all

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,8 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: 'postgresql://iconic_db_7haq_user:nZLqrHUVe4tW81Xi7Sna3S6LnjiL6VGv@dpg-d5a528emcj7s73c68vu0-a.frankfurt-postgres.render.com/iconic_db_7haq',
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
 
